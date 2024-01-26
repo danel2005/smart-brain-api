2d5358d8ef403db0fccbfc83ad40774636cbe3cf
danel2005 <dcoolmineduck@gmail.com>
2024-01-27 00:41:30 +0200

    final changes for the live db

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+DATABASE_URL = "postgres://danel2005:sOi7wdxWnouAdPNfaygjWsJzF84bqyEi@dpg-cmq0ahacn0vc73btsjf0-a/smart_brain_db_2f4i";
+DATABASE_HOST =  "dpg-cmq0ahacn0vc73btsjf0-a.oregon-postgres.render.com";
+DATABASE_USER = "danel2005";
+DATABASE_PW = "sOi7wdxWnouAdPNfaygjWsJzF84bqyEi";
+DATABASE_DB = "smart_brain_db_2f4i";
+
 const db = require('knex')({
     client: 'pg', // Change to 'pg' for PostgreSQL
     connection: {
-      host : 'dpg-cmq0ahacn0vc73btsjf0-a.oregon-postgres.render.com',
+      connectionString: process.env.DATABASE_URL,
+      ssl: {rejectUnauthorized: false},
+      host : process.env.DATABASE_HOST,
       port : 5432,
-      user : 'danel2005',
-      password : 'sOi7wdxWnouAdPNfaygjWsJzF84bqyEi',
-      database : 'smart_brain_db_2f4i'
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PW,
+      database : process.env.DATABASE_DB
     }
 });
 
@@ -24,26 +32,6 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'Sally@gmail.com',
-            password: 'oleg',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
 
 app.get('/', (req, res) => {
     res.send(database.users);
